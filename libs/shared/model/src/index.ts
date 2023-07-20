d8040ed8de97a43e1218d72e7344af60cb81cb87
Nikita Kovalev <nikita.kovalev.work@gmail.com>
2023-07-20 13:14:55 +0400

    build: fix helper

--- libs/shared/model/src/index.ts
+++ libs/shared/model/src/index.ts
@@ -1,1 +1,2 @@
 export {QueryParameters} from './interface/query-parameters';
+export {NumberInput} from './type/number-input';
--- libs/shared/util/src/lib/full-string.ts
+++ libs/shared/util/src/lib/full-string.ts
@@ -1,4 +1,4 @@
-import {NumberInput} from '@angular/cdk/coercion';
+import {NumberInput} from '@nx-angular-monorepo/shared/model';
 
 export const fullString = (list: NumberInput[] = [], separator: string = ' '): string => {
     return list.filter(Boolean).join(separator);
